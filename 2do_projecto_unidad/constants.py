67e8e5e905316b86288c9c46859d18def7deb675
Alice-png-27 <alishcoro@gmail.com>
2024-11-25 11:00:51 -0600

    Update constants.py
    ya estan escaladas las dimensiones de las constantes

--- 2do_projecto_unidad/constants.py
+++ 2do_projecto_unidad/constants.py
@@ -1,17 +1,16 @@
 import pygame
-
 pygame.init()
 
-WIDTH = 1000
-HEIGHT = 900
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
-pygame.display.set_caption('Two-Player Pygame Chess!')
-# These are the classic variables that we need to use for every game in 'pygame'. The 'WIDTH' and 'HEIGHT' correspond to the size of the screen that will pop up. 
-# The 'screen' variable is the place where we're going to draw everything that happens during the game. 
+WIDTH_INIT = 1000
+HEIGHT_INIT = 900
+WIDTH, HEIGHT = 800,720
 
-SMALL_FONT = pygame.font.Font('freesansbold.ttf', 20)
-MEDIUM_FONT = pygame.font.Font('freesansbold.ttf', 40)
-BIG_FONT = pygame.font.Font('freesansbold.ttf', 50)
+screen = pygame.display.set_mode([WIDTH, HEIGHT], pygame.RESIZABLE)
+pygame.display.set_caption('Two-Player Pygame Chess!')
+# These are the classic variables that we need to use for every game in 'pygame'. The 'WIDTH' and 'HEIGHT' correspond to the size of the screen that will pop up.
+# The 'screen' variable is the place where we're going to draw everything that happens during the game.
+
+
 # Even as the font is the same we put three different sizes. Why? Most probably for aesthetic purposes
 
 timer = pygame.time.Clock()
@@ -38,56 +37,70 @@ selection = 100
 # The 'selection' variable appears to just be an arbitrary number that will change depending on what we select.
 valid_moves = []
 
+def calcular_escala():
+    scale_x = screen.get_width() / WIDTH_INIT
+    scale_y = screen.get_height() / HEIGHT_INIT
+    return min(scale_x, scale_y)  # Escala uniforme para evitar distorsión
+def escalar(valor):
+    return int(valor * calcular_escala())
+def cargar_imagen(file, width, height):
+    image = pygame.image.load(file)
+    return pygame.transform.scale(image, (escalar(width), escalar(height)))
+def obtener_fuente(tamaño):
+    return pygame.font.Font('freesansbold.ttf', escalar(tamaño))
+
+SMALL_FONT = obtener_fuente(20)
+MEDIUM_FONT = obtener_fuente(40)
+BIG_FONT = obtener_fuente(50)
+
 # load in game piece images (queen, king, rook, bishop, knight, pawn) x 2
-black_queen = pygame.image.load("images/black_queen.png")
-black_queen = pygame.transform.scale(black_queen, (80, 80))
-black_queen_small = pygame.transform.scale(black_queen, (45, 45))
+black_queen = cargar_imagen("images/black_queen.png", 80, 80)
+
+black_queen_small = cargar_imagen("images/black_queen.png", 45, 45)
 # While it doesn't appear as an option to tab, to link a file to the method '.load()' I just need to put the name of the folder next to a ...
-# ... slash (/) and the file name and extension. 
+# ... slash (/) and the file name and extension.
 
-black_king = pygame.image.load("images/black_king.png")
-black_king = pygame.transform.scale(black_king, (80, 80))
-black_king_small = pygame.transform.scale(black_king, (45, 45))
+black_king = cargar_imagen("images/black_king.png", 80, 80)
+black_king_small = cargar_imagen("images/black_king.png", 45, 45)
 
-black_rook = pygame.image.load("images/black_rook.png")
-black_rook = pygame.transform.scale(black_rook, (80, 80))
-black_rook_small = pygame.transform.scale(black_rook, (45, 45))
+black_rook = cargar_imagen("images/black_rook.png", 80, 80)
+black_rook_small = cargar_imagen("images/black_rook.png", 45, 45)
 
-black_bishop = pygame.image.load("images/black_bishop.png")
-black_bishop = pygame.transform.scale(black_bishop, (80, 80))
-black_bishop_small = pygame.transform.scale(black_bishop, (45, 45))
 
-black_knight = pygame.image.load("images/black_knight.png")
-black_knight = pygame.transform.scale(black_knight, (80, 80))
-black_knight_small = pygame.transform.scale(black_knight, (45, 45))
+black_bishop = cargar_imagen("images/black_bishop.png", 80, 80)
+black_bishop_small = cargar_imagen("images/black_bishop.png", 45, 45)
 
-black_pawn = pygame.image.load("images/black_pawn.png")
-black_pawn = pygame.transform.scale(black_pawn, (65, 65))
-black_pawn_small = pygame.transform.scale(black_pawn, (45, 45))
 
-white_queen = pygame.image.load("images/white_queen.png")
-white_queen = pygame.transform.scale(white_queen, (80, 80))
-white_queen_small = pygame.transform.scale(white_queen, (45, 45))
+black_knight = cargar_imagen("images/black_knight.png", 80, 80)
+black_knight_small = cargar_imagen("images/black_knight.png", 45, 45)
 
-white_king = pygame.image.load("images/white_king.png")
-white_king = pygame.transform.scale(white_king, (80, 80))
-white_king_small = pygame.transform.scale(white_king, (45, 45))
 
-white_rook = pygame.image.load("images/white_rook.png")
-white_rook = pygame.transform.scale(white_rook, (80, 80))
-white_rook_small = pygame.transform.scale(white_rook, (45, 45))
+black_pawn = cargar_imagen("images/black_pawn.png", 65, 65)
+black_pawn_small = cargar_imagen("images/black_pawn.png", 45, 45)
 
-white_bishop = pygame.image.load("images/white_bishop.png")
-white_bishop = pygame.transform.scale(white_bishop, (80, 80))
-white_bishop_small = pygame.transform.scale(white_bishop, (45, 45))
 
-white_knight = pygame.image.load("images/white_knight.png")
-white_knight = pygame.transform.scale(white_knight, (80, 80))
-white_knight_small = pygame.transform.scale(white_knight, (45, 45))
+white_queen = cargar_imagen("images/white_queen.png", 80, 80)
+white_queen_small = cargar_imagen("images/white_queen.png", 45, 45)
 
-white_pawn = pygame.image.load("images/white_pawn.png")
-white_pawn = pygame.transform.scale(white_pawn, (65, 65))
-white_pawn_small = pygame.transform.scale(white_pawn, (45, 45))
+
+white_king = cargar_imagen("images/white_king.png", 80, 80)
+white_king_small = cargar_imagen("images/white_king.png", 45, 45)
+
+
+white_rook = cargar_imagen("images/white_rook.png", 80, 80)
+white_rook_small = cargar_imagen("images/white_rook.png", 45, 45)
+
+
+white_bishop = cargar_imagen("images/white_bishop.png", 80, 80)
+white_bishop_small = cargar_imagen("images/white_bishop.png", 45, 45)
+
+
+white_knight = cargar_imagen("images/white_knight.png", 80, 80)
+white_knight_small = cargar_imagen("images/white_knight.png", 45, 45)
+
+
+white_pawn = cargar_imagen("images/white_pawn.png", 65, 65)
+white_pawn_small = cargar_imagen("images/white_pawn.png", 45, 45)
 
 white_images = [white_pawn, white_queen, white_king, white_knight, white_rook, white_bishop]
 white_promotions = ['bishop', 'knight', 'rook', 'queen']
@@ -109,10 +122,9 @@ piece_list = ['pawn', 'queen', 'king', 'knight', 'rook', 'bishop']
 counter = 0
 winner = ''
 game_over = False
-white_ep = (100, 100)
-black_ep = (100, 100)
-# Again, two nonsense values. 
+white_ep = (escalar(100), escalar(100))
+black_ep = (escalar(100), escalar(100))
+# Again, two nonsense values.
 white_promote = False
 black_promote = False
 promo_index = 100
-# Using our favorite dummy value
